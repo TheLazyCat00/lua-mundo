fa39fb6b167d8457a7645e98d21e801456f4fac0
Manuel Stieger <manue.stieger@gmail.com>
2025-04-22 19:03:58 +0200

    WIP!

--- lua/lua-mundo/init.lua
+++ lua/lua-mundo/init.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.setup(opts)
 	if opts then
 		for key, value in pairs(opts) do
-			if value ~= nil and key:sub(1, 6) == "mundo_" then
-				vim.g[key] = value
+			if value ~= nil then
+				vim.g["mundo_" .. key] = value
 			end
 		end
 	end
